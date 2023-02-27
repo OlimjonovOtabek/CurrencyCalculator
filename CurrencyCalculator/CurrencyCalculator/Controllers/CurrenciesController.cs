fb9012970685ef3d2906d6d9496414357454b557
alimjanovf <almjnvfsystem@gmail.com>
2023-02-27 13:38:00 +0500

    Project Created

---
+++ CurrencyCalculator/CurrencyCalculator/Controllers/CurrenciesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyCalculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+    }
+}
